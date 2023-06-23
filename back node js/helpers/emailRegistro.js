9c66a16b87fe3154673a8f18bed2a30fb1216c0f
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-06-23 16:09:29 -0500

    version envio de correo

--- back node js/helpers/emailRegistro.js
+++ back node js/helpers/emailRegistro.js
@@ -3,17 +3,30 @@
 import nodemailer from 'nodemailer';
 
 
- const sendEmailRegistro = async (datos) => {
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-      });
 
-    console.log(datos);
+ const sendEmailRegistro = async (datos) => {
+  const  transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "a433f9495cc437",
+      pass: "22770eb1d92c9e"
+    }
+  });
+ 
+ 
+  const { email, nombre, token } = datos;
+
+  const info= await transport.sendMail({
+    from: "AppVeterinaria",
+    to: email,
+    subject: "Confirmación de registro",
+    html:  `<p>Hola ${nombre} comprueba tu cuenta</p>
+    <p> tu cuenta ya  esta lista, solo debe comprobarla en siguiente enlace</p>
+    <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Confirmar cuenta</a> 
+    `
+  })
+    console.log("mensaje enviado: %s", info.messageId);
 }
 
 export default sendEmailRegistro;

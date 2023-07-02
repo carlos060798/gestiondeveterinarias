3b1034e7db729f647a7ca2e884f469f72fdd6a96
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-07-01 20:58:48 -0500

    version listado pacientes

--- front reac js/frontend/src/components/ListadoPacientes.jsx
+++ front reac js/frontend/src/components/ListadoPacientes.jsx
@@ -1,7 +1,35 @@
+import usePacientes from "../hook/usePacientes";
+import Paciente from "./Paciente";
+
 function ListadoPaciente() {
-    return ( <>
-     <h1>list</h1>
-    </> );
+  const { pacientes } = usePacientes();
+  return (
+    <>
+      {pacientes.length ? (
+        <>
+          <h2 className=" font-black text-3xl text-center">
+            Lista de Pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600">Pacientes y citas</span>
+          </p>
+          {pacientes.map((paciente) => (
+            <Paciente key={paciente._id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className=" font-black text-3xl text-center">No Hay Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            {" "}
+            comienza agregando pacientes {""}
+            <span className="text-indigo-600">y apareceran en este lugar</span>
+          </p>
+        </>
+      )}
+    </>
+  );
 }
 
 export default ListadoPaciente;
--- front reac js/frontend/src/context/PacienteProvaider.jsx
+++ front reac js/frontend/src/context/PacienteProvaider.jsx
@@ -3,47 +3,70 @@
 import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
-const PacientesContext = createContext();
-export default  PacientesContext 
+const PacientesContext = createContext(); //context  para los componetes que nesesiten datos de los pacientes
+export default PacientesContext;
 
+const PacientesProvaider = ({ children }) => {
+  const [pacientes, setPacientes] = useState([]); //state para los pacientes
 
-
-const PacientesProvaider = ({children}) => {
-    const [pacientes, setPacientes] = useState([])
-    const GuardarPaciente= async(paciente)=>{
+    useEffect(() => { //useEffect para obtener los pacientes cuando se monte el componete
+      const obtenerPacientes = async () => { //funcion para obtener los pacientes de la lista 
         try {
-            const token= localStorage.getItem('token')             
-            const  config={
-                headers:{
-                    "Content-Type":"application/json",
-                    Authorization:`Bearer ${token}`
+          const token = localStorage.getItem("token"); //obtenemos el token del localstorage
+           if(!token) return; //si no hay token no hacemos nada
+            const config = {  
+                //configuracion para el axios
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
                 }
             }
+            const url = "http://localhost:4000/api/pacientes"; //url para el axios
+            const { data } = await axios.get(url, config); //hacemos la peticion get con axios
+            setPacientes(data); //agregamos los pacientes al state
+        }catch (err) {
+          console.log(err.response.data.msg);
+        }
 
-            const url = "http://localhost:4000/api/pacientes";
+      }
+        obtenerPacientes();
+    }, []);
 
-           const {data}= await axios.post(url, paciente, config);
-              console.log(data)
-              console.log(paciente)
-            
-           // setAlerta({ msg: "Cuenta creada correctamentamente", error: false });
-          } catch (err) {
-             console.log(err.response.data.msg)
-           //setAlerta({ msg:err.response.data.msg, error:true });
-          }
-        };
-        
-    
-    return (
-        <PacientesContext.Provider value={{
-            pacientes,
-            GuardarPaciente
-        }}>
-            {children}
-        </PacientesContext.Provider>
+  const GuardarPaciente = async (paciente) => {
+    //funcion para guardar los pacientes
+    try {
+      const token = localStorage.getItem("token"); //obtenemos el token del localstorage
+      const config = {
+        //configuracion para el axios
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
 
-    )
-}
+      const url = "http://localhost:4000/api/pacientes"; //url para el axios
 
+      const { data } = await axios.post(url, paciente, config); //hacemos la peticion post con axios
+      const { createdAt, updatedAt, __v, ...pacienteNuevo } = data; //desestructuramos la respuesta de la peticion post
+      setPacientes([pacienteNuevo, ...pacientes]); //agregamos el nuevo paciente al state
 
-export {PacientesProvaider}
+      // setAlerta({ msg: "Cuenta creada correctamentamente", error: false });
+    } catch (err) {
+      console.log(err.response.data.msg);
+      //setAlerta({ msg:err.response.data.msg, error:true });
+    }
+  };
+
+  return (
+    <PacientesContext.Provider
+      value={{
+        pacientes,
+        GuardarPaciente,
+      }}
+    >
+      {children}
+    </PacientesContext.Provider>
+  );
+};
+
+export { PacientesProvaider };
